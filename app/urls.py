00a57fa59cb6b6b0a788018b4fe7f39628a0e435
brijesh-py <134686367+brijesh-py@users.noreply.github.com>
2023-06-04 14:50:25 +0530

    Update urls.py
    route comment

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,9 @@ from .views import index, todo_delete, todo_update
 
 # home route
 app.add_url_rule('/', 'home', index, methods=['GET','POST'])
+# delete route
 app.add_url_rule('/delete/', 'delete', todo_delete, methods=['GET','POST'])
+# update route
 app.add_url_rule('/update/', 'update', todo_update, methods=['GET','POST'])
 
 
